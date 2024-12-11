a01e996f0fd35abdedf4a180e10063a15fb6f643
frankierdavid24 <frankierdavidalmanzar@gmail.com>
2024-12-11 11:28:04 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Aquí puedes agregar interactividad, como un menú desplegable o validación de formularios
+    console.log("Página cargada y lista para interactuar.");
+});
